d94eee87aeed07417800711742de22bb7f61c69a
Camilo Romero <camilo.romero@geniussportsmedia.com>
2021-10-25 17:55:30 -0500

    create chat with socket io

---
+++ models/messages.js
@@ -0,0 +1,15 @@
+
+//data base, table messages
+const messages = [
+    { author: "Juan", text: "¡Hola! ¿Que tal?" },
+    { author: "Pedro", text: "¡Muy bien! ¿Y vos?" },
+    { author: "Ana", text: "¡Genial!" }
+ ];
+
+ const getMessages= ()=>messages
+
+ const saveMessage = (message)=>{
+     messages.push(message)
+ }
+
+ module.exports = {getMessages, saveMessage}
